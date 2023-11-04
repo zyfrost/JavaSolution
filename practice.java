fe87c21c47b9186f655f10a738ee1c2286cdabdc
tanmay <murkutetanmay@gmail.com>
2023-11-04 19:24:59 +0530

    add priority queue

--- practice.java
+++ practice.java
@@ -1,36 +1,25 @@
 import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class practice {
 
   public static void main(String[] args) {
     /*
-     
-     * Queue - FIFO - First in First Out(ex. line of people)
-     * A collection prior to holding data prior to processing
-     * Linear data structure
-     * add - enqueue (adds at end), offer()
-     * remove - dequeue (remove from start),poll()
-     * name of datastructure <the elements stack can hold> var_name = create new
-     * instance of n.o.d<the element stack can hold> ();
-     * Queue is a Interface 
-     
+     * Priority Queue = FIFO Data Structure , that serves elements based with
+     * highest priority first before lower priority
      */
 
-    Queue<String> queue = new LinkedList<String>();
-    queue.offer("karen");// head
-    queue.offer("chad");
-    queue.offer("boomer");
-    queue.offer("Harold");// tail
-    System.out.println(queue.isEmpty());//false
-    System.out.println(queue.size());
-    System.out.println(queue.contains("Harold"));
+    Queue<Double> queue = new PriorityQueue<>();
 
-    System.out.println(queue);
-    queue.poll();//enqueue
-    // we can use element to peek but it causes exception "NoSuchElementException"
-    System.out.println(queue.peek());
+    queue.offer(3.0);
+    queue.offer(4.0);
+    queue.offer(2.5);
+    queue.offer(1.6);
+
+    while (!queue.isEmpty()) {
+      System.out.println(queue.poll());
+    }
 
     /*
      * Uses of Queue
