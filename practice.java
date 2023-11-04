e995388bae8d863e2ca9fcc5f703971cf54c5d3c
tanmay <murkutetanmay@gmail.com>
2023-11-04 10:45:22 +0530

    added queue files

--- practice.java
+++ practice.java
@@ -1,25 +1,43 @@
-import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class practice {
 
   public static void main(String[] args) {
-    // Stack - LIFO - Last in First Out
-    // it stores data in vertical tower form
-    // push() is to add at the top
-    // pop() is remove from the top
-    // name of datastructure <the elements stack can hold> var_name = create new
-    // instance of n.o.d<the element stack can hold> ();
-    Stack<String> stack = new Stack<String>();
-    stack.push("Minecraft");
-    stack.push("Doom");
-    stack.push("Eternal");
-    stack.push("Animal Crossing");
+    /*
+     
+     * Queue - FIFO - First in First Out(ex. line of people)
+     * A collection prior to holding data prior to processing
+     * Linear data structure
+     * add - enqueue (adds at end), offer()
+     * remove - dequeue (remove from start),poll()
+     * name of datastructure <the elements stack can hold> var_name = create new
+     * instance of n.o.d<the element stack can hold> ();
+     * Queue is a Interface 
+     
+     */
 
-    System.out.println(stack.peek());
-    //first position in stack starts from one
-        System.out.println(stack.search("Doom"));
+    Queue<String> queue = new LinkedList<String>();
+    queue.offer("karen");// head
+    queue.offer("chad");
+    queue.offer("boomer");
+    queue.offer("Harold");// tail
+    System.out.println(queue.isEmpty());//false
+    System.out.println(queue.size());
+    System.out.println(queue.contains("Harold"));
 
-    String myFavGame = stack.pop();
-    System.out.println(myFavGame);
+    System.out.println(queue);
+    queue.poll();//enqueue
+    // we can use element to peek but it causes exception "NoSuchElementException"
+    System.out.println(queue.peek());
+
+    /*
+     * Uses of Queue
+     * 1. keyboard buffer
+     * 2.Printer queue
+     * 3.used in linked list , priority queues, breath-first search
+     */
+
   }
 }
