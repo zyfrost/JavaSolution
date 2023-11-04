7fce90c6c41b8845edcc351505b3fc9f358795c6
tanmay <murkutetanmay@gmail.com>
2023-11-04 19:28:14 +0530

    add new file

---
+++ Data Structure/DataStructure/priorityqueue.java
@@ -0,0 +1,35 @@
+
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+
+public class priorityqueue {
+     public static void main(String[] args) {
+    /*
+     * Priority Queue = FIFO Data Structure , that serves elements based with
+     * highest priority first before lower priority
+     */
+
+    Queue<Double> queue = new PriorityQueue<>();
+
+    queue.offer(3.0);
+    queue.offer(4.0);
+    queue.offer(2.5);
+    queue.offer(1.6);
+
+    while (!queue.isEmpty()) {
+      System.out.println(queue.poll());
+    }
+
+    /*
+     * Uses of Queue
+     * 1. keyboard buffer
+     * 2.Printer queue
+     * 3.used in linked list , priority queues, breath-first search
+     */
+
+  }
+
+    
+}
